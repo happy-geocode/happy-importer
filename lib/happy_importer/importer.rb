2bed98c3be36981a48d00e8be16c6c8eae7927bb
Klaus Zanders <klaus.zanders@gmail.com>
2012-10-13 22:27:00 +0200

    Always check if the files exist
    This is useful so we can resume if something goes wrong, without having
    to go through the whole Osmosis export again

--- lib/happy_importer/importer.rb
+++ lib/happy_importer/importer.rb
@@ -30,50 +30,70 @@ module HappyImporter
 
       # Do the arangoimport magic here ... We do this manually for now
 
-      puts '[Extract Nodes] Deleting the temporary files!'
 
-      File.delete('/tmp/osm-nodes.xml')
       puts '[Extract Nodes] done!'
     end
 
     def extract_streets
-      puts "[Extract Streets] Osmosis is running to extract the streets"
-      `osmosis --read-xml file="#{filename}" --tag-filter reject-nodes --way-key keyList=highway --tag-filter accept-ways name="*" --tag-filter reject-relations --write-xml /tmp/osm-streets.xml`
+      if File.exist?('/tmp/osm-streets.xml')
+        puts '[Extract Streets] We already have the streets ... Skipping'
+      else
+        puts "[Extract Streets] Osmosis is running to extract the streets"
+        `osmosis --read-xml file="#{filename}" --tag-filter reject-nodes --way-key keyList=highway --tag-filter accept-ways name="*" --tag-filter reject-relations --write-xml /tmp/osm-streets.xml`
+      end
     end
 
     def extract_house_numbers
-      puts "[Extract House Numbers] Osmosis is running to extract the house number points"
-      `osmosis --read-xml file="#{@filename}" --tag-filter accept-nodes addr:housenumber="*" --tag-filter accept-relations addr:housenumber="*" --tag-filter accept-ways addr:housenumber="*" --write-xml /tmp/osm-house-numbers.xml`
+      if File.exist?('/tmp/osm-house-numbers.xml')
+        puts '[Extract House Numbers] We already have the house numbers ... Skipping'
+      else
+        puts "[Extract House Numbers] Osmosis is running to extract the house number points"
+
+        # Do not use nodes ... This will speed things up ... We just use the official places marked as house numbers in OpenStreetMap
+        `osmosis --read-xml file="#{@filename}" --tag-filter reject-nodes --tag-filter accept-relations addr:housenumber="*" --tag-filter accept-ways addr:housenumber="*" --write-xml /tmp/osm-house-numbers.xml`
+        #`osmosis --read-xml file="#{@filename}" --tag-filter accept-nodes addr:housenumber="*" --tag-filter accept-relations addr:housenumber="*" --tag-filter accept-ways addr:housenumber="*" --write-xml /tmp/osm-house-numbers.xml`
+      end
     end
 
     def extract_borders
-      puts "[Extract Temporary Borders] Osmosis is running to extract all the borders in the file"
-      `osmosis --read-xml file="#{@filename}" --tag-filter reject-nodes --tag-filter accept-relations boundary=administrative --tag-filter accept-ways boundary=administrative --write-xml /tmp/osm-borders.xml`
+      if File.exist?('/tmp/osm-borders.xml')
+        puts '[Extract Temporary Borders] We already have the borders ... Skipping'
+      else
+        puts "[Extract Temporary Borders] Osmosis is running to extract all the borders in the file"
+        `osmosis --read-xml file="#{@filename}" --tag-filter reject-nodes --tag-filter accept-relations boundary=administrative --tag-filter accept-ways boundary=administrative --write-xml /tmp/osm-borders.xml`
+      end
     end
 
     def extract_states
-      puts "[Extract States] Osmosis is running to extract the states from the temporary borders file"
-      `osmosis --read-xml file=/tmp/osm-borders.xml --tag-filter reject-nodes --tag-filter accept-relations admin_level=4 --tag-filter accept-ways admin_level=4 --write-xml /tmp/osm-states.xml`
-
-      puts "[Extract States] Osmosis is done ... Now we parse the XML"
-
+      if File.exist?('/tmp/osm-states.xml')
+        puts '[Extract States] We already have the state borders ... Skipping'
+      else
+        puts '[Extract States] Exrtracting the state borders from the temporary borders file"'
+        `osmosis --read-xml file=/tmp/osm-borders.xml --tag-filter reject-nodes --tag-filter accept-relations admin_level=4 --tag-filter accept-ways admin_level=4 --write-xml /tmp/osm-states.xml`
+        puts '[Extract States] Osmosis is done ... Now we parse the XML'
+      end
     end
 
     def extract_cities
-      puts "[Extract States] Osmosis is running to extract the cities from the temporary borders file"
-      `osmosis --read-xml file=/tmp/osm-borders.xml --tag-filter reject-nodes --tag-filter accept-relations admin_level=8 --tag-filter accept-ways admin_level=8 --write-xml /tmp/osm-cities.xml`
+      if File.exist?('/tmp/osm-cities.xml')
+        puts '[Extract Cities] We already have the city borders ... Skipping'
+      else
+        puts "[Extract Cities] Osmosis is running to extract the city borders from the temporary borders file"
+        `osmosis --read-xml file=/tmp/osm-borders.xml --tag-filter reject-nodes --tag-filter accept-relations admin_level=8 --tag-filter accept-ways admin_level=8 --write-xml /tmp/osm-cities.xml`
+      end
     end
 
     def extract_zip_codes
       # Bodo Magic
     end
 
-    private
-    def arango_connection
-      if @arango_host
-        @arango ||= ::Ashikawa::Core::Database.new @arango_host
-      else
-        nil
+     # Cleanup all the shit
+    def cleanup
+      files = %w{/tmp/osm-nodes.xml /tmp/osm-streets.xml /tmp/osm-house-numbers.xml /tmp/osm-borders.xml /tmp/osm-states.xml /tmp/osm-cities.xml /tmp/temp-nodes.csv}
+
+      files.each do |file|
+        puts "[Cleanup] #{file}"
+        File.delete(file) if File.exist? file
       end
     end
   end
