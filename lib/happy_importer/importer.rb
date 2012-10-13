a0e08ae4f2952fa0cf8a39feb2701f35432f6c12
Klaus Zanders <klaus.zanders@gmail.com>
2012-10-13 18:37:43 +0200

    Renamed osmosis.rake to import.rake

--- lib/happy_importer/importer.rb
+++ lib/happy_importer/importer.rb
@@ -22,27 +22,36 @@ module HappyImporter
       puts "[Extract Nodes] Now we are parsing the XML and putting everything in the sqlite db ... Please stand by (this might take a while)"
       parser = ::Nokogiri::XML::SAX::Parser.new(Document::NodeDocument.new(sqlite_connection))
       parser.parse File.open('/tmp/osm-nodes.xml', 'r')
+
+      puts '[Extract Nodes] Deleting the temporary files!'
+
+      File.delete('/tmp/osm-nodes.xml')
       puts '[Extract Nodes] done!'
     end
 
     def extract_streets
-      # osmosis --read-xml file="koeln-regbez.osm" --tag-filter reject-nodes --way-key keyList=highway --tag-filter accept-ways name="*" --tag-filter reject-relations --write-xml koeln-strassen.osm
+      puts "[Extract Streets] Osmosis is running to extract the streets"
+      `osmosis --read-xml file="#{filename}" --tag-filter reject-nodes --way-key keyList=highway --tag-filter accept-ways name="*" --tag-filter reject-relations --write-xml /tmp/osm-streets.xml`
     end
 
     def extract_house_numbers
-      # osmosis --read-xml file="koeln-regbez.osm" --tag-filter accept-nodes addr:housenumber="*" --tag-filter accept-relations addr:housenumber="*" --tag-filter accept-ways addr:housenumber="*" --write-xml koeln-hausnummern.osm
+      puts "[Extract House Numbers] Osmosis is running to extract the house number points"
+      `osmosis --read-xml file="#{@filename}" --tag-filter accept-nodes addr:housenumber="*" --tag-filter accept-relations addr:housenumber="*" --tag-filter accept-ways addr:housenumber="*" --write-xml /tmp/osm-house-numbers.xml`
     end
 
     def extract_borders
-      # osmosis --read-xml file="koeln-regbez.osm" --tag-filter reject-nodes --tag-filter accept-relations boundary=administrative --tag-filter accept-ways boundary=administrative --write-xml koeln-grenzen.osm
+      puts "[Extract Temporary Borders] Osmosis is running to extract all the borders in the file"
+      `osmosis --read-xml file="#{@filename}" --tag-filter reject-nodes --tag-filter accept-relations boundary=administrative --tag-filter accept-ways boundary=administrative --write-xml /tmp/osm-borders.xml`
     end
 
     def extract_states
-      # osmosis --read-xml file=koeln-grenzen.osm --tag-filter reject-nodes --tag-filter accept-relations admin_level=4 --tag-filter accept-ways admin_level=4 --write-xml koeln-bundesland.osm
+      puts "[Extract States] Osmosis is running to extract the states from the temporary borders file"
+      `osmosis --read-xml file=/tmp/osm-borders.xml --tag-filter reject-nodes --tag-filter accept-relations admin_level=4 --tag-filter accept-ways admin_level=4 --write-xml /tmp/osm-states.xml`
     end
 
     def extract_cities
-      # osmosis --read-xml file=koeln-grenzen.osm --tag-filter reject-nodes --tag-filter accept-relations admin_level=8 --tag-filter accept-ways admin_level=8 --write-xml koeln-staedte.osm
+      puts "[Extract States] Osmosis is running to extract the cities from the temporary borders file"
+      `osmosis --read-xml file=/tmp/osm-borders.xml --tag-filter reject-nodes --tag-filter accept-relations admin_level=8 --tag-filter accept-ways admin_level=8 --write-xml /tmp/osm-cities.xml`
     end
 
     def extract_zip_codes
