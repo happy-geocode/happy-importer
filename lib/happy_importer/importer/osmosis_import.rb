d086e5be5a4485cb97555137284f3d157744985a
Klaus Zanders <klaus.zanders@gmail.com>
2012-10-14 12:59:00 +0200

    Debug output

--- lib/happy_importer/importer/osmosis_import.rb
+++ lib/happy_importer/importer/osmosis_import.rb
@@ -45,7 +45,7 @@ module HappyImporter
 
       def extract_streets
         # if File.exist?('/tmp/osm-streets.xml')
-        #   puts "#{Time.new} [Extract Streets] We already have the streets ... Skipping"
+        #  puts "#{Time.new} [Extract Streets] We already have the streets ... Skipping"
         # else
         #   puts "#{Time.new} [Extract Streets] Osmosis is running to extract the streets"
         #   `osmosis --read-xml file="#{filename}" --tag-filter reject-nodes --way-key keyList=highway --tag-filter accept-ways name="*" --tag-filter reject-relations --write-xml /tmp/osm-streets.xml`
@@ -54,7 +54,7 @@ module HappyImporter
         puts "#{Time.new} [Extract Streets] The parser is starting to parse the streets"
         doc = Document::OsmStreetsDocument.new
         parser = ::Nokogiri::XML::SAX::Parser.new(doc)
-        parser.parse File.open('/tmp/osm/koeln-strassen.osm')
+        parser.parse File.open('/tmp/osm/koeln-strassen.osm') # TODO: Ue the right temp file here
 
         puts "#{Time.new} [Extract Streets] The parser is done with the streets osm ... No we have to connect the streets that belong together"
 
@@ -75,49 +75,53 @@ module HappyImporter
         end
 
         puts "#{Time.new} [Extract Streets] Done with the connection ... Saving everything"
+        puts "#{Time.new} [Extract Streets] Done!"
       end
 
-      def extract_house_numbers
-        if File.exist?('/tmp/osm-house-numbers.xml')
-          puts '[Extract House Numbers] We already have the house numbers ... Skipping'
-        else
-          puts "[Extract House Numbers] Osmosis is running to extract the house number points"
+      # def extract_house_numbers
+      #   if File.exist?('/tmp/osm-house-numbers.xml')
+      #     puts "#{Time.new} [Extract House Numbers] We already have the house numbers ... Skipping"
+      #   else
+      #     puts "#{Time.new} [Extract House Numbers] Osmosis is running to extract the house number points"
 
-          # Do not use nodes ... This will speed things up ... We just use the official places marked as house numbers in OpenStreetMap
-          `osmosis --read-xml file="#{@filename}" --tag-filter reject-nodes --tag-filter accept-relations addr:housenumber="*" --tag-filter accept-ways addr:housenumber="*" --write-xml /tmp/osm-house-numbers.xml`
-          #`osmosis --read-xml file="#{@filename}" --tag-filter accept-nodes addr:housenumber="*" --tag-filter accept-relations addr:housenumber="*" --tag-filter accept-ways addr:housenumber="*" --write-xml /tmp/osm-house-numbers.xml`
-        end
-      end
+      #     # Do not use nodes ... This will speed things up ... We just use the official places marked as house numbers in OpenStreetMap
+      #     `osmosis --read-xml file="#{@filename}" --tag-filter reject-nodes --tag-filter accept-relations addr:housenumber="*" --tag-filter accept-ways addr:housenumber="*" --write-xml /tmp/osm-house-numbers.xml`
+      #     #`osmosis --read-xml file="#{@filename}" --tag-filter accept-nodes addr:housenumber="*" --tag-filter accept-relations addr:housenumber="*" --tag-filter accept-ways addr:housenumber="*" --write-xml /tmp/osm-house-numbers.xml`
+      #   end
+      # end
 
       def extract_borders
         if File.exist?('/tmp/osm-borders.xml')
-          puts '[Extract Temporary Borders] We already have the borders ... Skipping'
+          puts "#{Time.new} [Extract Temporary Borders] We already have the borders ... Skipping"
         else
-          puts "[Extract Temporary Borders] Osmosis is running to extract all the borders in the file"
+          puts "#{Time.new} [Extract Temporary Borders] Osmosis is running to extract all the borders in the file"
           `osmosis --read-xml file="#{@filename}" --tag-filter reject-nodes --tag-filter accept-relations boundary=administrative --tag-filter accept-ways boundary=administrative --write-xml /tmp/osm-borders.xml`
+          puts "#{Time.new} [Extract Temporary Borders] Done!"
         end
       end
 
       def extract_states
         if File.exist?('/tmp/osm-states.xml')
-          puts '[Extract States] We already have the state borders ... Skipping'
+          puts "#{Time.new} [Extract States] We already have the state borders ... Skipping"
         else
-          puts '[Extract States] Exrtracting the state borders from the temporary borders file"'
+          puts "#{Time.new} [Extract States] Extracting the state borders from the temporary borders file"
           `osmosis --read-xml file=/tmp/osm-borders.xml --tag-filter reject-nodes --tag-filter accept-relations admin_level=4 --tag-filter accept-ways admin_level=4 --write-xml /tmp/osm-states.xml`
-          puts '[Extract States] Osmosis is done ... Now we parse the XML'
+          puts "#{Time.new} [Extract States] Osmosis is done ... Now we parse the XML"
           import = HappyImporter::Importer::OsmImport.new("/tmp/osm-states.xml", "state")
           import.extract_osm
+          puts "#{Time.new} [Extract States] Done!"
         end
       end
 
       def extract_cities
         if File.exist?('/tmp/osm-cities.xml')
-          puts '[Extract Cities] We already have the city borders ... Skipping'
+          puts "#{Time.new} [Extract Cities] We already have the city borders ... Skipping"
         else
-          puts "[Extract Cities] Osmosis is running to extract the city borders from the temporary borders file"
+          puts "#{Time.new} [Extract Cities] Osmosis is running to extract the city borders from the temporary borders file"
           `osmosis --read-xml file=/tmp/osm-borders.xml --tag-filter reject-nodes --tag-filter accept-relations admin_level=8 --tag-filter accept-ways admin_level=8 --write-xml /tmp/osm-cities.xml`
           import = HappyImporter::Importer::OsmImport.new("/tmp/osm-cities.xml", "city")
           import.extract_osm
+          puts "#{Time.new} [Extract Cities] Done!"
         end
       end
 
